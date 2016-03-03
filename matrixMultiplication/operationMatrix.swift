7b2584f9abb81f1b34b402a74c7d0f228c758ec0
Toby Applegate <up661724@myport.ac.uk>
2016-03-03 10:02:12 +0000

    deleted commented code

--- matrixMultiplication/operationMatrix.swift
+++ matrixMultiplication/operationMatrix.swift
@@ -18,108 +18,40 @@ class operationMatrix {
         let operationQueue = NSOperationQueue()
         
         func calculateMatrix() {
-            
-            self.testArrayOne = [ [1,2,3] , [4,5,6] ]
-            self.testArrayTwo = [ [7,9,11] , [8,10,12] ]
-            
+
             mapArrayFromString("/Users/tobyapplegate/Desktop/arrayOne", arrayName: "matrixArrayOne")
             mapArrayFromString("/Users/tobyapplegate/Desktop/arrayTwo", arrayName: "matrixArrayTwo")
             
-            print(matrixArrayOne.count)
-            print(matrixArrayTwo.count)
-            //multiplyMatricie()
-            
             let startTime = NSDate()
             
-//            let arrayOne1 = matrixArrayOne[0 ... 99]
-//            let arrayOne2 = matrixArrayOne[100 ... 199]
-//            let arrayOne3 = matrixArrayOne[200 ... 299]
-//            let arrayOne4 = matrixArrayOne[300 ... 399]
-//            
-//            let arrayTwo1 = matrixArrayTwo[0 ... 99]
-//            let arrayTwo2 = matrixArrayTwo[100 ... 199]
-//            let arrayTwo3 = matrixArrayTwo[200 ... 299]
-//            let arrayTwo4 = matrixArrayTwo[300 ... 399]
+            let processes = 32
             
-            let count = 32
-            
-            let segments = matrixArrayOne.count / count // 400 / 4
+            let segments = matrixArrayOne.count / processes // how many processes we want to use
             let dividerArray = Array(0.stride(to: matrixArrayOne.count + 1, by: segments))
-            
-            print("divider array = \(dividerArray)")
-            
+
             for var i = 0; i < dividerArray.count; i++ {
-                print("i = \(i)")
-                print(dividerArray.count-1)
                 if i != dividerArray.count-1 {
-                    print(i)
                     let arrayOne = matrixArrayOne[dividerArray[i] ... (dividerArray[i+1]-1)]
                     let arrayTwo = matrixArrayTwo[dividerArray[i] ... (dividerArray[i+1]-1)]
                     addToQue(arrayOne, arrayTwo: arrayTwo, startTime: startTime, operNumber: i)
                 }
-//                else {
-//                    let arrayOne = matrixArrayOne[dividerArray[i] ... (dividerArray[i+1]-1)]
-//                    let arrayTwo = matrixArrayTwo[dividerArray[i] ... (dividerArray[i+1]-1)]
-//                    addToQue(arrayOne, arrayTwo: arrayTwo, startTime: startTime, operNumber: i)
-//                }
-
-
             }
     }
     
-    func addToQue(arrayOne: ArraySlice<[Int]>, arrayTwo: ArraySlice<[Int]>, startTime: NSDate, operNumber: Int){
-        
+    func addToQue(arrayOne: ArraySlice<[Int]>, arrayTwo: ArraySlice<[Int]>, startTime: NSDate, operNumber: Int) {
         let operation = NSBlockOperation() {
             self.multiplyMatricie(arrayOne, maTwo: arrayTwo)
         }
         operation.completionBlock = {
-            print("done opperatoin \(operNumber)")
+            //print("done operation \(operNumber)")
             self.printTime(startTime)
         }
         operationQueue.addOperation(operation)
     }
     
-//            let op1 = NSBlockOperation() {
-//                self.multiplyMatricie(arrayOne1, maTwo: arrayTwo1)
-//            }
-//            
-//            op1.completionBlock = {
-//                print("done op1")
-//                self.printTime(startTime)
-//            }
-//           
-//            let op2 = NSBlockOperation() {
-//                self.multiplyMatricie(arrayOne2, maTwo: arrayTwo2)
-//            }
-//            
-//            op2.completionBlock = {
-//                print("done op2")
-//                self.printTime(startTime)
-//            }
-//            
-//            let op3 = NSBlockOperation() {
-//                self.multiplyMatricie(arrayOne3, maTwo: arrayTwo3)
-//            }
-//            
-//            op3.completionBlock = {
-//                print("done op3")
-//                self.printTime(startTime)
-//            }
-//            
-//            let op4 = NSBlockOperation() {
-//                self.multiplyMatricie(arrayOne4, maTwo: arrayTwo4)
-//            }
-//            
-//            op4.completionBlock = {
-//                print("done op4")
-//                self.printTime(startTime)
-//            }
-//            
-//            operationQueue.addOperations([op1,op2,op3,op4], waitUntilFinished: false)
-        //}
-    
-    func printTime(startTime: NSDate){
+    func printTime(startTime: NSDate) {
         if operationQueue.operationCount == 0 {
+            print("operationsCount = \(operationQueue.operationCount)")
             let endTime = NSDate()
             let timeInterval: Double = endTime.timeIntervalSinceDate(startTime) // <<<<< Difference in seconds (double)
             print("Time to complete operation version: \(timeInterval/0.001) milli-seconds")
@@ -152,7 +84,6 @@ class operationMatrix {
         }
     
     func multiplyMatricie(maOne:  ArraySlice<[Int]>, maTwo:  ArraySlice<[Int]>) {
-
             var total = 0
             for matrixOne in maOne {
                 for matrixTwo in maTwo {
@@ -171,7 +102,6 @@ class operationMatrix {
                 print(testArrayOne![index][index])
             }
         }
-    
 }
 
 extension Array {
@@ -182,22 +112,3 @@ extension Array {
         }
     }
 }
-
-
-//multiply the two matricies together
-
-//    func divideArray(array: String, increment: Int) {
-//        if array == "matrixArrayOne" {
-//            let size = matrixArrayOne.count/increment
-//            print(size)
-////            let strideAmount = strid
-//            for index in stride(from: 0, to: matrixArrayOne.count, by: size) {
-//                print("index : \(index)")
-//
-//            }
-//        }
-//        else {
-//
-//        }
-//    }
-    
