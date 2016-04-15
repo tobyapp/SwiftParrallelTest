0ac7fd240043707edc2f6d51e9673aa1663817e9
Toby Applegate <up661724@myport.ac.uk>
2016-04-15 15:51:18 +0100

    working multiplying matricies with test/example matricies

--- matrixMultiplication/TestOperationMatrix.swift
+++ matrixMultiplication/TestOperationMatrix.swift
@@ -11,71 +11,65 @@ import Cocoa
 
 class TestOperationMatrix {
     
-    var testArrayOne = [ [1,2,3] , [4,5,6], [7,8,9] , [10,11,12], [13,14,15] , [16,17,18], [19,20,21] , [22,23,24], [1,2,3] , [4,5,6], [7,8,9] , [10,11,12], [13,14,15] , [16,17,18], [19,20,21] , [22,23,24] ]
-    var testArrayTwo = [ [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12] ]
+//    var testArrayOne = [ [1,2,3], [4,5,6], [7,8,9], [10,11,12], [13,14,15], [16,17,18], [19,20,21], [22,23,24], [25,26,27], [28,29,30], [31,32,33], [34,35,36], [37,38,39] , [40,41,42], [43,44,45] , [46,47,48]]
+//    var testArrayTwo = [ [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12], [7,9,11] , [8,10,12]]
     
+    var testArrayOne = [ [1,2,3], [4,5,6]]
+    var testArrayTwo = [ [7,9,11] , [8,10,12]]
     var totalArray = [Int]()
-
-    let numberOfArrays = 2
     let operationQueue = NSOperationQueue()
-
-//    func splitMatrix(splitBy: Int, arrayToSplit: [Array<Int>]) {
-//        let x = arrayToSplit.count / splitBy
-//        let tempArrayOne = arrayToSplit[]
-//    }
     
     func calculateMatrix() {
 
         let startTime = NSDate()
-        let processes = 4
-        let segments = testArrayOne.count / processes // how many processes we want to use
-        //let dividerArray = Array(0.stride(to: testArrayOne.count + 1, by: segments))
-   
-
+        let processes = 2
         
-        //print("divider array = \(dividerArray)")
+        let splitArrayOne = splitMatrix(processes: processes, matrix: testArrayOne)
+        let splitArrayTwo = splitMatrix(processes: processes, matrix: testArrayTwo)
         
-//        for i in 0 ..< dividerArray.count {
-//            if i != dividerArray.count-1 {
-//                let arrayOne = testArrayOne[dividerArray[i] ... (dividerArray[i+1]-1)]
-//                //print("arrayOne : \(arrayOne)")
-//                let arrayTwo = testArrayTwo[dividerArray[i] ... (dividerArray[i+1]-1)]
-//                //print("arrayTwo : \(arrayTwo)")
-//                addToQue(arrayOne[0], arrayTwo: arrayTwo[0], startTime: startTime, operationNumber: i)
-//            }
-        
-        splitMatrix(processes: processes)
+        //iterate through the two nested arrays of matricies and multiply them e.g. 1.1 * 2.1, 1.1 * 2.2, 1.2 * 2.1, 1.2 * 2.2 (see link for more info)
+        var operationNumberCount = 1
+        for count in 0..<splitArrayOne.count {
+            for count1 in 0..<splitArrayTwo.count {
+//                print(count)
+//                print(splitArrayOne[count])
+//                print(splitArrayTwo[count])
+                addToQue(splitArrayOne[count], arrayTwo: splitArrayTwo[count1], startTime: startTime, operationNumber: operationNumberCount)
+                operationNumberCount += 1
+            }
+        }
     }
     
-    func splitMatrix(processes numberOfArrays: Int) { //, matrixOne : [Array<Int>], matrixTwo: [Array<Int>]
-        let numOfelementsInArray = testArrayOne.count / numberOfArrays
+    func splitMatrix(processes numberOfArrays: Int, matrix array: [Array<Int>]) -> [[Array<Int>]]{ //, matrixOne : [Array<Int>], matrixTwo: [Array<Int>]
+        let numOfelementsInArray = array.count / numberOfArrays
         var matrixElement = 0
         var tempArray = [[Array<Int>]]()
-        print("number of elements in each array is : \(numOfelementsInArray)")
+        //print("number of elements in each array is : \(numOfelementsInArray)")
         for i in 0 ..< numberOfArrays {
-            print("array number : \(i+1)")
+            //print("array number : \(i+1)")
             var tempMatrix = [Array<Int>]()
             for _ in 0 ..< numOfelementsInArray {
-                tempMatrix.append(testArrayOne[matrixElement])
+                tempMatrix.append(array[matrixElement])
                 matrixElement = matrixElement + 1
             }
             //print(tempMatrix)
             tempArray.append(tempMatrix)
         }
-        print(tempArray)
+        return tempArray
     }
     
-    func addToQue(arrayOne: [Int], arrayTwo: [Int], startTime: NSDate, operationNumber: Int) {
+    func addToQue(arrayOne: [Array<Int>], arrayTwo: [Array<Int>], startTime: NSDate, operationNumber: Int) {
         let operation = NSBlockOperation() {
-            print("going to multiply \(arrayOne) by \(arrayTwo)")
-            //self.multiplyMatricie(arrayOne, maTwo: arrayTwo)
-            self.multiplySingleMatricie(arrayOne, maTwo: arrayTwo)
+            self.multiplyMatricie(arrayOne, maTwo: arrayTwo)
         }
         operation.completionBlock = {
             //print("done operation \(operNumber)")
             self.printTime(startTime)
+            print(self.totalArray)
         }
         operationQueue.addOperation(operation)
+        print("operationsCount = \(operationQueue.operationCount)")
+
     }
     
     // multiply two single matricies
@@ -90,19 +84,17 @@ class TestOperationMatrix {
     
     func printTime(startTime: NSDate) {
         if operationQueue.operationCount == 0 {
-            //print("operationsCount = \(operationQueue.operationCount)")
             let endTime = NSDate()
             let timeInterval: Double = endTime.timeIntervalSinceDate(startTime) // <<<<< Difference in seconds (double)
             print("Time to complete operation version: \(timeInterval/0.001) milli-seconds")
         }
     }
     
-    func multiplyMatricie(maOne: ArraySlice<[Int]>, maTwo: ArraySlice<[Int]>) {
+    func multiplyMatricie(maOne: [Array<Int>], maTwo: [Array<Int>]) {
         var total = 0
         for matrixOne in maOne {
             for matrixTwo in maTwo {
                 for index in 0 ..< matrixOne.count {
-                    print("multiplying \(matrixOne[index]) by \(matrixTwo[index])")
                     if index == 0 {
                         total = 0
                     }
@@ -110,15 +102,11 @@ class TestOperationMatrix {
                     //prints results
                     if index  == (matrixOne.count - 1) || index == (matrixTwo.count - 1) {
                         print("total : \(total)")
+                        totalArray.append(total)
                     }
                 }
             }
         }
     }
 
-    func test() {
-        for var index = 0; index < testArrayOne.count; ++index {
-            print(testArrayOne[index][index])
-        }
-    }
 }
